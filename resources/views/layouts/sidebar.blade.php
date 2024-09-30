8ce66fb3f654c220edb1f628fa6a8540cac7fd36
Muhammad Raffa <azraff1982@gmail.com>
2024-09-30 08:14:50 +0700

    ASIDE BENER COY

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -19,19 +19,20 @@
             </svg>
             <span class="mx-3">Dashboard</span>
         </a>
-        <a class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+
+        <a class="flex items-center text-decoration-none style-none px-6 py-2 mt-4 text-base font-normal hover:bg-gray-700 hover:bg-opacity-25 text-gray-500 hover:text-gray-100 w-full transition duration-75 group"
             href="/resident-table">
-            <svg class="w-6 h-6 flex-shrink-0  text-gray-500 transition duration-100 group-hover:text-gray-100 dark:text-gray-400 dark:group-hover:text-white"
-                xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24"
-                xmlns:xlink="http://www.w3.org/1999/xlink">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            <svg class="w-6 h-6 flex-shrink-0 text-gray-600 transition duration-100 group-hover:text-white dark:text-gray-400 dark:group-hover:text-white"
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path
                     d="m.213,9.145c-.341-.435-.264-1.063.171-1.404L8.919,1.062c1.814-1.419,4.348-1.42,6.162,0l8.535,6.679c.435.34.512.969.171,1.404-.197.252-.491.384-.788.384-.215,0-.433-.069-.615-.212L13.849,2.638c-1.088-.852-2.609-.852-3.697,0L1.616,9.316c-.436.34-1.063.262-1.403-.171Zm3.524,8.89c-2.166.591-3.737,2.679-3.737,4.965,0,.553.447,1,1,1s1-.447,1-1c0-1.379.973-2.684,2.263-3.035.533-.146.848-.695.702-1.228-.146-.534-.699-.847-1.228-.702Zm16.525,0c-.526-.146-1.082.168-1.228.702-.146.532.169,1.082.702,1.228,1.29.352,2.263,1.656,2.263,3.035,0,.553.447,1,1,1s1-.447,1-1c0-2.286-1.571-4.374-3.737-4.965Zm-15.763-7.035c-1.381,0-2.5,1.119-2.5,2.5s1.119,2.5,2.5,2.5,2.5-1.119,2.5-2.5-1.119-2.5-2.5-2.5Zm17.5,2.5c0-1.381-1.119-2.5-2.5-2.5s-2.5,1.119-2.5,2.5,1.119,2.5,2.5,2.5,2.5-1.119,2.5-2.5Zm-10-5.5c-1.381,0-2.5,1.119-2.5,2.5s1.119,2.5,2.5,2.5,2.5-1.119,2.5-2.5-1.119-2.5-2.5-2.5Zm0,7c-2.757,0-5,2.243-5,5v3c0,.553.447,1,1,1s1-.447,1-1v-3c0-1.654,1.346-3,3-3s3,1.346,3,3v3c0,.553.447,1,1,1s1-.447,1-1v-3c0-2.757-2.243-5-5-5Z" />
             </svg>
             <span class="mx-3">Data Penduduk</span>
         </a>
-        <a class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+
+        <a class="flex items-center text-decoration-none style-none px-6 py-2 mt-4 text-base font-normal hover:bg-gray-700 hover:bg-opacity-25 text-gray-500 hover:text-gray-100 w-full transition duration-75 group"
             href="/resident-migration">
-            <svg class="w-6 h-6 flex-shrink-0  text-gray-500 transition duration-100 group-hover:text-gray-100 dark:text-gray-400 dark:group-hover:text-white"
+            <svg class="w-6 h-6 flex-shrink-0 text-gray-600 transition duration-100 group-hover:text-white dark:text-gray-400 dark:group-hover:text-white"
                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path
                     d="M12 2C11.4477 2 11 2.44772 11 3V5C11 5.55228 11.4477 6 12 6H15C16.1046 6 17 6.89543 17 8V9.58579L19.2929 7.29289C19.6834 6.90237 20.3166 6.90237 20.7071 7.29289C21.0976 7.68342 21.0976 8.31658 20.7071 8.70711L17 12.4142L13.2929 8.70711C12.9024 8.31658 12.9024 7.68342 13.2929 7.29289C13.6834 6.90237 14.3166 6.90237 14.7071 7.29289L17 9.58579V8C17 7.44772 16.5523 7 16 7H13V3C13 2.44772 12.5523 2 12 2ZM7 7C8.10457 7 9 7.89543 9 9V11H7C5.89543 11 5 11.8954 5 13V16C5 17.1046 5.89543 18 7 18H9V19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19V17C3.89543 17 3 16.1046 3 15V13C3 11.8954 3.89543 11 5 11V9C5 7.89543 5.89543 7 7 7Z" />
@@ -80,7 +81,7 @@
         @if (auth()->check() && Auth::user()->role_id === 1)
             <a class="{{ request()->is('users') ? 'bg-gray-700 bg-opacity-25' : '' }} mt-4 flex items-center px-6 py-2 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
                 href="/user-management">
-                <svg class="w-6 h-6 fill-white" xmlns="http://www.w3.org/2000/svg" height="20" width="17.5"
+                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" height="20" width="17.5" fill="currentColor"
                     viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                     <path
                         d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464l349.5 0c-8.9-63.3-63.3-112-129-112l-91.4 0c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3z" />
